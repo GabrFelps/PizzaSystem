33deb74bfa8f0814291ca709e9c0930459d99e11
GabrFelps <felipemrqs07@gmail.com>
2025-02-07 04:38:43 -0300

    feat: DB rodando na nuvem

--- backend-pizzaria/config/db.js
+++ backend-pizzaria/config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
+// Carregando as variáveis de ambiente
+require('dotenv').config();
+
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/pizzaria', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB conectado!");
+        // Conectando ao MongoDB usando a URI do Atlas a partir do .env
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB conectado ao Atlas!");
     } catch (error) {
         console.error("Erro ao conectar ao MongoDB:", error);
-        process.exit(1);
+        process.exit(1);  // Finaliza o processo em caso de erro
     }
 };
 
